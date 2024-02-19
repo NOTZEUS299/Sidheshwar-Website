8d371f5b596290ca082a5a97ba336692b2a45a65
Darshan Viradiya <drshnptl137@gmail.com>
2024-02-19 18:59:35 +0530

    Update header component styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center w-full bg-[#fefcf5] h-[48px] min-[751px]:h-[56px] min-[1001px]:h-[82px] px-[20px] min-[1001px]:px-[36px] min-[751px]:absolute top-0 z-50" style={{borderBottom:"1px solid #232323"}}>
+    <header className="flex justify-between items-center w-full bg-[#fefcf5] h-[48px] min-[751px]:h-[56px] min-[1001px]:h-[82px] px-[20px] min-[1001px]:px-[36px] fixed top-0 z-50" style={{borderBottom:"1px solid #232323"}}>
       <div className="text-black w-[40%] min-[751px]:w-[20%] h-auto" data-sidheshwar-logo>
         <Link href="/">
             <Image src="/assets/sidheshwarlogo.png" alt="Sidheshwar" width={100} height={100} className="w-[100px] min-[751px]:w-[120px] min-[1001px]:w-[150px]" />
