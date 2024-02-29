e771d8ef88627e5773b3e66013189237a4a5d36b
NOTZEUS <drshnptl137@gmail.com>
2024-02-29 17:31:43 +0530

    Add image hover functionality and update UI layout

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from 'next/image';
 import React, { useEffect, useState, useRef } from 'react';
 
 const Index = () => {
@@ -6,7 +7,12 @@ const Index = () => {
   const [prevY, setPrevY] = useState<number>(0);
   const [tiltDirection, setTiltDirection] = useState<string>('');
   const [cursorSpeed, setCursorSpeed] = useState<number>(0); // Added cursorSpeed state
-  const imageRef = useRef<HTMLDivElement>(null);
+  const [hoverImageOne, setHoverImageOne] = useState<boolean>(false);
+  const imageContainerOneRef = useRef<any>(null);
+  const imageOneRef = useRef<any>(null);
+  const [hoverImageTwo, setHoverImageTWo] = useState<boolean>(false);
+  const imageContainerTwoRef = useRef<any>(null);
+  const imageTwoRef = useRef<any>(null);
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
@@ -23,9 +29,15 @@ const Index = () => {
       setPrevX(clientX);
       setPrevY(clientY);
 
-      if (imageRef.current) {
-        imageRef.current.style.left = `${clientX}px`;
-        imageRef.current.style.top = `${clientY}px`;
+      if (imageOneRef.current) {
+        imageOneRef.current.style.left = `${clientX}px`;
+        imageOneRef.current.style.top = `${clientY}px`;
+        imageOneRef.current.style.transform = `translate(-50%, -100%) rotate(${tiltDirection === "tilt-left" ? -((Math.ceil(cursorSpeed) - 1) * 3.5) : ((Math.ceil(cursorSpeed) - 1) * 3.5)}deg)`;
+      }
+      if (imageTwoRef.current) {
+        imageTwoRef.current.style.left = `${clientX}px`;
+        imageTwoRef.current.style.top = `${clientY}px`;
+        imageTwoRef.current.style.transform = `translate(-50%, -200%) rotate(${tiltDirection === "tilt-left" ? -((Math.ceil(cursorSpeed) - 1) * 3.5) : ((Math.ceil(cursorSpeed) - 1) * 3.5)}deg)`;
       }
     };
 
@@ -37,32 +49,22 @@ const Index = () => {
   }, [prevX]);
 
   return (
-    <div className='overflow-x-hidden' style={{ width: '100%', height: 'auto', backgroundColor: 'black' }}>
-
-      <div id="second" className='w-full h-[100vh]'>
-        <div className="elem">
-          <img
-            className={`absolute w-[500px] z-50`}
-            ref={imageRef}
-            src="/assets/gradientThree.jpg"
-            alt=""
-            style={{ transition: 'all 0.1s ease', transform: `translate(-50%, -50%) rotate(${tiltDirection === "tilt-left" ? -((Math.ceil(cursorSpeed) - 1) * 3.5) : ((Math.ceil(cursorSpeed) - 1) * 3.5)}deg)` }}
-          />
-          <h1>The Plug</h1>
-          <h5>2022</h5>
-        </div>
-        <div className="elem">
-          <img src="./assets/gradientOne.jpg" className='w-[100px] hidden' alt="" />
-          <h1>ixperience</h1>
-          <h5>2022</h5>
-        </div>
-        <div className="elem elemlast">
-          <img src="./assets/gradientOne.jpg" className='w-[100px] hidden' alt="" />
-          <h1>hudu</h1>
-          <h5>2022</h5>
-        </div>
-      </div>
-    </div>
+    <ul className='w-full h-fit pt-32' >
+      <li ref={imageContainerOneRef} className='w-full h-[250px] bg-[#f3f3f3] relative flex justify-evenly items-center' style={{ borderBottom: "1px solid #232323" }} onMouseOver={() => console.log(true)} onMouseLeave={() => console.log(false)}>
+        <h2 className='text-[2.5vw] text-[#232323] opacity-50'>1</h2>
+        <h2 className='text-[2.5vw] text-[#232323] opacity-50'>Graphic Design ~ Ui/Ux</h2>
+        <p className='w-[30%] text-[#232323] opacity-60'>Maxime aut odit reprehenderit atque accusantium corporis dolorem qui eius voluptatibus fuga, earum tempora corrupti et? Eveniet, molestiae quaerat!</p>
+        <Image width={400} height={350} ref={hoverImageOne ? imageOneRef : null} className={`absolute left-0 top-0 w-[400px] ${hoverImageOne ? 'opacity-100' : 'opacity-0'}`} src="/assets/gradientThree.jpg" objectFit="cover" style={{ transition: "all .1s ease" }} alt="" />
+        <h2 className='text-[3vw] text-[#232323] opacity-50'>↗</h2>
+      </li>
+      <li className='w-full h-[250px] bg-[#f3f3f3] relative flex justify-evenly items-center' style={{ borderBottom: "1px solid #232323" }}>
+        <h2 className='text-[2.5vw] text-[#232323] opacity-50'>1</h2>
+        <h2 className='text-[2.5vw] text-[#232323] opacity-50'>Coding ~ Web & App</h2>
+        <p className='w-[30%] text-[#232323] opacity-60'>Maxime aut odit reprehenderit atque accusantium corporis dolorem qui eius voluptatibus fuga, earum tempora corrupti et? Eveniet, molestiae quaerat!</p>
+        {/* <Image className={`absolute w-[400px] ${hoverImageOne ? 'opacity-100' : 'opacity-0'}`} src="/assets/gradientThree.jpg" layout="fill" objectFit="cover" style={{ transition: "all .1s ease" }} alt="" /> */}
+        <h2 className='text-[3vw] text-[#232323] opacity-50'>↗</h2>
+      </li>
+    </ul>
   );
 };
 
